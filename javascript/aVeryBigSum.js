367e3f23e9de1495e0c1f19edc5006f6b574c40d
jbaxter6 <jbaxter6@radford.edu>
2020-10-05 11:07:54 -0400

    aVeryBigSum

---
+++ javascript/aVeryBigSum.js
@@ -0,0 +1,15 @@
+function aVeryBigSum(ar) {
+    // first set a sum of 0
+    // then loop through array and add each iter to sum
+    let sum = 0;
+    for(let i = 0; i < ar.length; i++){
+        sum += ar[i];
+    }
+    return sum;
+}
+
+// input
+// [5, 1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
+
+// expected output
+// 5000000015
