3e68170fe65f1200f5347efa4f6477d6aef43c54
jbaxter6 <jbaxter6@radford.edu>
2020-10-06 00:43:53 -0400

    staircase

---
+++ javascript/staircase.js
@@ -0,0 +1,17 @@
+function staircase(n) {
+const hash = '#'
+    for(let i = 0; i <= n; i++){
+        return hash.repeat(i).padStart(n)
+        // .padStart starts char at number
+    }  
+}
+
+// n = 3
+//    #
+//   ##
+//  ###
+
+not 
+// #
+// ##
+// ###
