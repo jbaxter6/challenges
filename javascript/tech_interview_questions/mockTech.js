ca90ec341ace6f41ed76ba7b565a404072338e38
Johnnathan Baxter <jbaxter6@radford.edu>
2020-11-01 18:38:30 -0500

    Update mockTech.js

--- javascript/tech_interview_questions/mockTech.js
+++ javascript/tech_interview_questions/mockTech.js
@@ -1,7 +1,7 @@
 // given a length of a flight in minutes, and an array of movie lengths,
 // return true if two different movies can be equal to exactly the flight length
-// So findMovies(160, [80, 20, 40]) returns false 
-// but findMovies(160, [80, 80, 60]) returns true
+// So findMovies(160, [80, 20, 40]) returns false. 
+// but findMovies(160, [80, 80, 60]) returns true.
 
 function findMovies(target, arr){
     // first we are going to create an object to store the complement of the iteration in the array.
